7f0b98c5977f416a9a85ff7374ffc663f1b1b872
Akkadius <akkadius1@gmail.com>
2017-09-15 05:03:17 -0500

    Re-UI Snapshots page

--- public/assets/js/glass-pages/dhcp-config-snapshots.js
+++ public/assets/js/glass-pages/dhcp-config-snapshots.js
@@ -1,13 +1,16 @@
 function view_snapshot(snapshot) {
 	$.post( "/dhcp_config_snapshot_view", "snapshot=" + encodeURIComponent(snapshot), function( data ) {
 
-		$('#modal-title').html("Snapshot '" + snapshot + "'");
-		$('#modal-body').html('<div id="snapshot" style="width:100%; height:800px; color: #95cd24">' + data + '</div>');
+		$('#snapshot_grid').fadeOut(100).fadeIn(100);
+		$('#snapshot_name').html("Snapshot '" + snapshot + "'");
+		$('#snapshot_body').html('<div id="snapshot" style="width:100%; height:800px; color: #95cd24">' + data + '</div>');
+
+        $('html, body').animate({
+            scrollTop: $("#snapshot_grid").offset().top
+        }, 500);
 
 		config_snapshot = ace.edit("snapshot");
 		config_snapshot.setTheme("ace/theme/terminal");
 		config_snapshot.$blockScrolling = Infinity;
-
-		$('#mdModal').modal('show');
 	});
 }
--- routes/dhcp_config_snapshots.js
+++ routes/dhcp_config_snapshots.js
@@ -4,6 +4,30 @@ var fs = require('fs');
 var template_render = require('../lib/render_template.js');
 var authorize = require('../lib/authorize.js');
 
+function human_time (time){
+    var time = new Date(time);
+    var year = time.getFullYear();
+    var month = time.getMonth()+1;
+    var date1 = time.getDate();
+    var hour = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    var amPM = (hour > 11) ? "PM" : "AM";
+    if(hour > 12) {
+        hour -= 12;
+    } else if(hour == 0) {
+        hour = "12";
+    }
+    if(minute < 10) {
+        minute = "0" + minute;
+    }
+
+    return year + "-" + month+"-"+date1+" "+hour + ":" + minute + ' ' + amPM;
+}
+
 router.get('/', authorize.auth, function(req, res, next) {
 
 	var content = "";
@@ -18,7 +42,10 @@ router.get('/', authorize.auth, function(req, res, next) {
 
 	var backups = '';
 	fs.readdirSync("./config_backups").forEach(function(file) {
-		backups = backups + "<li><a onclick='view_snapshot(" + JSON.stringify(file) + ")'>" + file + '</a></li>';
+        var stats = fs.statSync("./config_backups/" + file);
+        var mtime = human_time(stats.mtime);
+
+		backups = backups + "<tr><td><a style='cursor:pointer;' onclick='view_snapshot(" + JSON.stringify(file) + ")'>" + file + '</a></td><td>' + mtime + '</td></tr>';
 	});
 
 	if(backups == ''){
