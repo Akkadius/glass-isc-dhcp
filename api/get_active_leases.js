f492600de208517a92137195dbb1dcb301b523df
Akkadius <akkadius1@gmail.com>
2017-10-05 13:01:50 -0500

    Add optional search param to API call GET: /api/get_active_leases?search=Cisco

--- api/get_active_leases.js
+++ api/get_active_leases.js
@@ -9,6 +9,37 @@ router.get('/', function(req, res, next) {
 	var lease_parser = require('../lib/lease_parser.js');
 	lease_parser.clean();
 
+	var dhcp_lease_data_return_buffer = {};
+
+	var search_string = req.query.search;
+	if (typeof search_string !== "undefined"){
+
+        for (var key in dhcp_lease_data) {
+
+            var matcher = new RegExp(search_string, "i");
+
+            if(
+                !matcher.test(dhcp_lease_data[key].mac_oui_vendor)
+                && !matcher.test(dhcp_lease_data[key].host)
+                && !matcher.test(key)
+                && !matcher.test(dhcp_lease_data[key].mac)
+            )
+                continue;
+
+
+            if(typeof dhcp_lease_data_return_buffer[key] !== "undefined")
+                dhcp_lease_data_return_buffer[key] = {};
+
+            dhcp_lease_data_return_buffer[key] = dhcp_lease_data[key];
+
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(dhcp_lease_data_return_buffer));
+
+        return true;
+	}
+
 	res.setHeader('Content-Type', 'application/json');
 	res.send(JSON.stringify(dhcp_lease_data));
 });
