b590ccfaf9cb9ccf404cd7d73a7751e076740877
mn.albeschenko <mn.albeschenko@tensor.ru>
2021-05-12 17:01:18 +0300

    Fix async start bug(OUI), fix
    1. When Glass starting, OUI base not loaded, cause concurency. Therefore the vendor is not defined.
    
    2. When you have failover dhcp cluster, there is many junk records.

--- core/oui-reader.js
+++ core/oui-reader.js
@@ -1,27 +1,29 @@
 var fs = require('fs');
 
 module.exports = {
-	initOuiDatabase: function () {
-		var oui_database_file = "bin/oui_table.txt";
-
-		if (fs.existsSync(oui_database_file)) {
-			fs.readFile(oui_database_file, 'utf8', function (err, data) {
-				if (err) {
-					return console.log(err);
-				}
-				else {
-					lines = data.split("\n");
-					for (l = 0; l < lines.length; l++) {
-						/* Trim whitespaces at each ends of the line */
-						lines[l]          = lines[l].trim();
-						var oui_line_data = lines[l].split(":::");
-
-						if (typeof oui_line_data[1] !== "undefined")
-							oui_data[oui_line_data[0].trim()] = oui_line_data[1].trim();
+	initOuiDatabase: async function ()  {
+		return new Promise((resolve) => {
+			var oui_database_file = "bin/oui_table.txt";
+			if (fs.existsSync(oui_database_file)) {
+				fs.readFile(oui_database_file, 'utf8', function (err, data) {
+					if (err) {
+						console.log(err);
 					}
-					console.log("[Glass Server] OUI Database Loaded");
-				}
-			});
-		}
-	},
-};
+					else {
+						lines = data.split("\n");
+						for (l = 0; l < lines.length; l++) {
+							/* Trim whitespaces at each ends of the line */
+							lines[l]          = lines[l].trim();
+							var oui_line_data = lines[l].split(":::");
+	
+							if (typeof oui_line_data[1] !== "undefined")
+								oui_data[oui_line_data[0].trim()] = oui_line_data[1].trim();
+						}
+						console.log("[Glass Server] OUI Database Loaded");
+						resolve()
+					}
+				});
+			}
+		})
+	}
+}
